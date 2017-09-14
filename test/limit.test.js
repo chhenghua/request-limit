601ea14d41d9f82f4310900e851138732603b897
chenghua <chehua263859@163.com>
2017-09-14 17:45:00 +0800

    redis添加支持

--- test/limit.test.js
+++ test/limit.test.js
@@ -1,31 +1,52 @@
 
 'use strict'
 
+const redis = require('redis')
+const client = redis.createClient(6379, '127.0.0.1')
+
 const limit = require('../index')
 
-// describe('test/limit.test.js', function() {
-//     describe('MemoryStore', function() {
-//         it('should request 200', async function(done) {
-//             console.log(await limit.checkLimit({
-//                 limit: 10,
-//                 interval: 200000
-//             }, 'token111', '/api/user/getList'))
-//             await limit.record()
-//         })
-//     })
-// })
+const urlMap = new Map()
+urlMap.set('/api/user/getList', {limit: 2, interval: 20})
+urlMap.set('/api/user/add', {limit: 2, interval: 2})
+urlMap.set('/api/user/getOne', {limit: 2, interval: 2})
+
+limit.init(urlMap, {
+    store: {
+        host: '127.0.0.1',
+        port: 6379
+    }
+})
 
 const method = async () => {
-    const rlt = await limit.checkLimit({
-        limit: 10,
-        interval: 200000
-    }, 'token111', '/api/user/getList')
-    await limit.record()
+    const rlt = await limit.checkLimit('token111', '/api/user/getList')
+    console.log('rlt@@@@@@@@@@'.repeat(10))
     console.log(rlt)
+    await limit.record('token111', '/api/user/getList')
+    return rlt
 }
 
-method()
-method()
-method()
-method()
-method()
+const testMethod = () => {
+    return (async () => {
+        await method()
+    })
+}
+
+// testMethod()
+// testMethod()
+// testMethod()
+// testMethod()
+// testMethod()
+
+;(async () => {
+    await method()
+})()
+// ;(async () => {
+//     await method()
+// })()
+// ;(async () => {
+//     await method()
+// })()
+// ;(async () => {
+//     await method()
+// })()
